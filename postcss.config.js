f31b795e0610b79f311899e378a1fc596b722da6
breetherkildsen <bree.therkildsen@gmail.com>
2021-02-04 11:12:42 -0800

    add eslint

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
 const tailwindcss = require('tailwindcss');
 
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.config.js'),
-        require('autoprefixer'),
-    ],
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer'),
+  ],
 };
